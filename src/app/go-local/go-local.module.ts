eefba4b7bf992875a56c8c7631103fcad1028b63
Carol B. Sempere <carolbrazalessempere@gmail.com>
2024-05-06 19:35:11 +0200

    Finish home components

--- src/app/go-local/go-local.module.ts
+++ src/app/go-local/go-local.module.ts
@@ -4,9 +4,10 @@ import { RouterModule } from '@angular/router';
 import { GlCardGuidesComponent } from './components/gl-card-guides/gl-card-guides.component';
 import { HomeComponent } from './pages/home/home.component';
 import { SharedModule } from '../shared/shared.module';
+import { CardPostComponent } from './components/card-post/card-post.component';
 
 @NgModule({
-  declarations: [GlCardGuidesComponent, HomeComponent],
+  declarations: [GlCardGuidesComponent, HomeComponent, CardPostComponent],
   imports: [CommonModule, RouterModule, SharedModule],
   exports: [GlCardGuidesComponent, HomeComponent],
 })
