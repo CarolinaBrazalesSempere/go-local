6f20d1fc685447924d01b76f2c6ff31718643297
Ainara <ainaradepablo@gmail.com>
2024-05-09 15:38:26 +0200

    opinion y tarjeta guia

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -5,10 +5,12 @@ import { RouterModule } from '@angular/router';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { SearchBoxComponent } from './components/search-box/search-box.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { OpinionComponent } from './components/opinion/opinion.component';
+import { TarjetaGuiaComponent } from './components/tarjeta-guia/tarjeta-guia.component';
 
 @NgModule({
-  declarations: [NavBarComponent, SearchBoxComponent, FooterComponent],
+  declarations: [NavBarComponent, SearchBoxComponent, FooterComponent, OpinionComponent,TarjetaGuiaComponent ],
   imports: [CommonModule, RouterModule],
-  exports: [NavBarComponent, SearchBoxComponent, FooterComponent],
+  exports: [NavBarComponent, SearchBoxComponent, FooterComponent, OpinionComponent,TarjetaGuiaComponent ],
 })
 export class SharedModule {}
