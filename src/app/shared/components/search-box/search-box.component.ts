4dd7ea2105c93328b810c5865a12e945bf097924
Carol B. Sempere <carolbrazalessempere@gmail.com>
2024-05-14 19:36:13 +0200

    Remove unused import

--- src/app/shared/components/search-box/search-box.component.ts
+++ src/app/shared/components/search-box/search-box.component.ts
@@ -1,10 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {
-  FormBuilder,
-  FormControl,
-  FormGroup,
-  Validators,
-} from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'shared-search-box',
