0d55d8862229929ee572db09c488eecb82c8cfc3
Manuel Sanchis Tormo <manuelsanchistormo@gmail.com>
2024-05-23 12:19:21 +0200

    add blog entries  links

--- src/app/go-local/pages/articulo/articulo.component.ts
+++ src/app/go-local/pages/articulo/articulo.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PostBlog } from '../../interfaces/PostBlog';
+import { ActivatedRoute } from '@angular/router';
+import { BlogService } from '../blog/services/blog.service';
 
 @Component({
   selector: 'app-articulo',
   templateUrl: './articulo.component.html',
   styleUrls: ['./articulo.component.css']
 })
-export class ArticuloComponent {
+export class ArticuloComponent implements OnInit {
+
+  article: PostBlog | undefined;
+
+  constructor(
+    private route: ActivatedRoute,
+    private blogService: BlogService
+  ) {}
+
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('idPost');
+    if (id) {
+      this.blogService.getBlogEntryById(+id).subscribe(data => {
+        this.article = data;
+      });
+    }
+  }
 
 }
--- src/app/go-local/pages/blog/services/blog.service.ts
+++ src/app/go-local/pages/blog/services/blog.service.ts
@@ -6,12 +6,18 @@ import { PostBlog } from 'src/app/go-local/interfaces/PostBlog';
 @Injectable({providedIn: 'root'})
 export class BlogService {
 
-  private baseUrl = 'http://localhost:8083/blog/todos';
+  private baseUrl = 'http://localhost:8083/blog';
 
   constructor(private http: HttpClient) { }
 
   getBlogEntries(): Observable<PostBlog[]> {
-    return this.http.get<PostBlog[]>(this.baseUrl);
+    // return this.http.get<PostBlog[]>(this.baseUrl);
+    return this.http.get<PostBlog[]>(`${this.baseUrl}/todos`);
+  }
+
+  getBlogEntryById(id: number): Observable<PostBlog> {
+    // return this.http.get<PostBlog>(`${this.baseUrl}/articulo/${id}`);
+    return this.http.get<PostBlog>(`${this.baseUrl}/${id}`);
   }
 
 }
