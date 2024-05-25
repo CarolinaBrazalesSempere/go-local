f9971882cf054c9f3375972727cb73d4a19956e3
Carol <carolbrazalessempere@gmail.com>
2024-05-25 13:48:47 +0200

    Send data from front to back when a user signs up

--- src/app/go-local/pages/signup/signup.component.ts
+++ src/app/go-local/pages/signup/signup.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
+import { SignupService } from './signup.service';
+
+import { UserSignUp } from '../../interfaces/UserSignUp';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.css']
+  styleUrls: ['./signup.component.css'],
 })
 export class SignupComponent {
+  user: UserSignUp = {
+    nombre: '',
+    apellidos: '',
+    email: '',
+    dni: '',
+    telefono: '',
+    sobreMi: '',
+    username: '',
+    contrasena: '',
+  };
+  constructor(private signUp: SignupService) {}
 
+  onSubmit() {
+    this.signUp.onRegister(this.user).subscribe(
+      (response) => {
+        console.log('True', response);
+      },
+      (error) => {
+        console.error('Error', error);
+      }
+    );
+  }
 }
---
+++ src/app/go-local/pages/signup/signup.service.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { UserSignUp } from '../../interfaces/UserSignUp';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class SignupService {
+  private apiUrl = 'http://localhost:8083/signup';
+  constructor(private http: HttpClient) {}
+
+  onRegister(user: UserSignUp): Observable<UserSignUp> {
+    return this.http.post<UserSignUp>(this.apiUrl, user);
+  }
+}
