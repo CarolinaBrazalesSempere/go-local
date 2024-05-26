7e5c3213058419cbaeb4bf8543dca61760efc72e
Manuel Sanchis Tormo <manuelsanchistormo@gmail.com>
2024-05-26 18:25:16 +0200

    add average reviews to card guides

--- src/app/go-local/components/gl-card-guides/gl-card-guides.component.ts
+++ src/app/go-local/components/gl-card-guides/gl-card-guides.component.ts
@@ -14,17 +14,23 @@ export class GlCardGuidesComponent implements OnInit {
   guideCity: { [key: number]: string } = {};
   guides: Guia[] = [];
   displayedGuides: Guia[] = [];
+  averageReviews: { [key: number]: number } = {};
 
   constructor(private apiService: ApiService) {}
 
   ngOnInit(): void {
     this.apiService.getGuides().subscribe(data => {
-      this.guides = data.sort((a, b) => b.idGuia - a.idGuia).slice(0, 4);
+      this.guides = data.sort((a, b) => a.idGuia - b.idGuia).slice(0, 4);
       this.displayedGuides = this.guides;
 
       this.guides.forEach(guia => {
+        // Carga la ciudad de cada guía
         this.apiService.getCiudadByGuiaId(guia.idGuia).subscribe(ciudad => {
           this.guideCity[guia.idGuia] = ciudad.nombreCiudad;
+        });
+        // Carga la media de puntuación de cada guía
+        this.apiService.getMediaPuntuacionByGuiaId(guia.idGuia).subscribe(media => {
+          this.averageReviews[guia.idGuia] = media;
         });
       });
     });
