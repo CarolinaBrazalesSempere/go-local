9e9d7f59a0a3a7c7694cbe0e85afbe9ab33af9ee
Manuel Sanchis Tormo <manuelsanchistormo@gmail.com>
2024-05-27 19:09:25 +0200

    add home guide card button features

--- src/app/go-local/components/book-guide-btn/book-guide-btn.component.ts
+++ src/app/go-local/components/book-guide-btn/book-guide-btn.component.ts
@@ -1,8 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'gl-book-guide-btn',
   templateUrl: './book-guide-btn.component.html',
   styleUrls: ['./book-guide-btn.component.css'],
 })
-export class BookGuideBtnComponent {}
+export class BookGuideBtnComponent {
+  @Input() idGuia!: number;
+
+  constructor(private router: Router) {}
+
+  goToFichaGuia(): void {
+    console.log(this.idGuia);
+    this.router.navigateByUrl(`/ficha-guia/${this.idGuia}`);
+  }
+}
