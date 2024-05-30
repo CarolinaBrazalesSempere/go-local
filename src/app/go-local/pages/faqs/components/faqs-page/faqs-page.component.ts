cdbd42419f0ca554172cadf2feb53017537cd156
Manuel Sanchis Tormo <manuelsanchistormo@gmail.com>
2024-05-30 12:23:45 +0200

    clean faqs pages  component

--- src/app/go-local/pages/faqs/components/faqs-page/faqs-page.component.ts
+++ src/app/go-local/pages/faqs/components/faqs-page/faqs-page.component.ts
@@ -1,14 +1,14 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component } from '@angular/core';
+
 import { FaqsModalService } from '../../services/faqs-modal.service';
 
 @Component({
   selector: 'app-faqs-page',
   templateUrl: './faqs-page.component.html',
-  styleUrls: ['./faqs-page.component.css']
+  styleUrls: ['./faqs-page.component.css'],
 })
 export class FaqsPageComponent {
-
-  constructor(private faqsModalService: FaqsModalService) { }
+  constructor(private faqsModalService: FaqsModalService) {}
 
   openModal() {
     this.faqsModalService.showModal();
