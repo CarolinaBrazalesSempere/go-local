b9fe3f2423bae15dfee363f0b73fae08d7876a1b
Manuel Sanchis Tormo <manuelsanchistormo@gmail.com>
2024-05-30 12:18:58 +0200

    clean blog service

--- src/app/go-local/pages/blog/services/blog.service.ts
+++ src/app/go-local/pages/blog/services/blog.service.ts
@@ -1,23 +1,20 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+
 import { PostBlog } from 'src/app/go-local/interfaces/PostBlog';
 
-@Injectable({providedIn: 'root'})
+@Injectable({ providedIn: 'root' })
 export class BlogService {
-
   private baseUrl = 'http://localhost:8083/blog';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   getBlogEntries(): Observable<PostBlog[]> {
-    // return this.http.get<PostBlog[]>(this.baseUrl);
     return this.http.get<PostBlog[]>(`${this.baseUrl}/todos`);
   }
 
   getBlogEntryById(id: number): Observable<PostBlog> {
-    // return this.http.get<PostBlog>(`${this.baseUrl}/articulo/${id}`);
     return this.http.get<PostBlog>(`${this.baseUrl}/${id}`);
   }
-
 }
