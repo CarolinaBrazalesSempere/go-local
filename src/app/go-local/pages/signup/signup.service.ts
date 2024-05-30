f7e50a0383d491e5eadc35519166880244be19a4
Manuel Sanchis Tormo <manuelsanchistormo@gmail.com>
2024-05-30 12:44:35 +0200

    clean singup service

--- src/app/go-local/pages/signup/signup.service.ts
+++ src/app/go-local/pages/signup/signup.service.ts
@@ -1,7 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+
 import { UserSignUp } from '../../interfaces/UserSignUp';
+
 
 @Injectable({
   providedIn: 'root',
