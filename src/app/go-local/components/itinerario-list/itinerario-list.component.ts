ae40ce30b49449979677f6d58d695fa42ab51be7
Manuel Sanchis Tormo <manuelsanchistormo@gmail.com>
2024-05-30 12:04:12 +0200

    itinerario list  component

--- src/app/go-local/components/itinerario-list/itinerario-list.component.ts
+++ src/app/go-local/components/itinerario-list/itinerario-list.component.ts
@@ -1,16 +1,14 @@
 import { Component } from '@angular/core';
-import { ApiService } from 'src/app/api.service';
+
 import { Itinerario } from '../../interfaces/itinerario';
 
 @Component({
-  selector: 'app-itinerario-list',
+  selector: 'gl-itinerario-list',
   templateUrl: './itinerario-list.component.html',
   styleUrls: ['./itinerario-list.component.css'],
 })
 export class ItinerarioListComponent {
   itinerarios: Itinerario[] = [];
-
-  constructor(private apiService: ApiService) {}
 
   ngOnInit(): void {}
 }
