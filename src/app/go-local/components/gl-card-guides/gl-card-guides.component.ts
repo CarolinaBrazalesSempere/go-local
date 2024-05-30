e77fd1283d3cb1ab2bf5b97f576a922931857ad2
Manuel Sanchis Tormo <manuelsanchistormo@gmail.com>
2024-05-30 12:00:56 +0200

    clean gl card guides  component

--- src/app/go-local/components/gl-card-guides/gl-card-guides.component.ts
+++ src/app/go-local/components/gl-card-guides/gl-card-guides.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { Itinerario } from '../../interfaces/itinerario';
+
 import { ApiService } from 'src/app/api.service';
 import { Guia } from '../../interfaces/Guia';
+
 
 @Component({
   selector: 'gl-card-guides',
@@ -9,8 +10,6 @@ import { Guia } from '../../interfaces/Guia';
   styleUrls: ['./gl-card-guides.component.css'],
 })
 export class GlCardGuidesComponent implements OnInit {
-
-
   guideCity: { [key: number]: string } = {};
   guides: Guia[] = [];
   displayedGuides: Guia[] = [];
@@ -19,21 +18,21 @@ export class GlCardGuidesComponent implements OnInit {
   constructor(private apiService: ApiService) {}
 
   ngOnInit(): void {
-    this.apiService.getGuides().subscribe(data => {
+    this.apiService.getGuides().subscribe((data) => {
       this.guides = data.sort((a, b) => a.idGuia - b.idGuia).slice(0, 4);
       this.displayedGuides = this.guides;
 
-      this.guides.forEach(guia => {
-        // Carga la ciudad de cada guía
-        this.apiService.getCiudadByGuiaId(guia.idGuia).subscribe(ciudad => {
+      this.guides.forEach((guia) => {
+        this.apiService.getCiudadByGuiaId(guia.idGuia).subscribe((ciudad) => {
           this.guideCity[guia.idGuia] = ciudad.nombreCiudad;
         });
-        // Carga la media de puntuación de cada guía
-        this.apiService.getMediaPuntuacionByGuiaId(guia.idGuia).subscribe(media => {
-          this.averageReviews[guia.idGuia] = media;
-        });
+
+        this.apiService
+          .getMediaPuntuacionByGuiaId(guia.idGuia)
+          .subscribe((media) => {
+            this.averageReviews[guia.idGuia] = media;
+          });
       });
     });
   }
 }
-
