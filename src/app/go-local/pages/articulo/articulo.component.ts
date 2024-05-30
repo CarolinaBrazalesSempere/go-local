44ab1f36c3faf79580dcdd6f65f256e86c43e2e3
Manuel Sanchis Tormo <manuelsanchistormo@gmail.com>
2024-05-30 12:13:42 +0200

    clean page  articulo

--- src/app/go-local/pages/articulo/articulo.component.ts
+++ src/app/go-local/pages/articulo/articulo.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
-import { PostBlog } from '../../interfaces/PostBlog';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+
 import { BlogService } from '../blog/services/blog.service';
+import { PostBlog } from '../../interfaces/PostBlog';
 
 @Component({
   selector: 'app-articulo',
   templateUrl: './articulo.component.html',
-  styleUrls: ['./articulo.component.css']
+  styleUrls: ['./articulo.component.css'],
 })
 export class ArticuloComponent implements OnInit {
-
   article: PostBlog | undefined;
 
   constructor(
@@ -19,10 +19,10 @@ export class ArticuloComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-    this.route.params.subscribe(params => {
+    this.route.params.subscribe((params) => {
       const id = params['idPost'];
       if (id) {
-        this.blogService.getBlogEntryById(+id).subscribe(data => {
+        this.blogService.getBlogEntryById(+id).subscribe((data) => {
           this.article = data;
         });
       }
