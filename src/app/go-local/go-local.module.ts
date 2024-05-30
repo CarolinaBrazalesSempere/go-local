3bc5bd7be577bd30396557545f38fc5a134e13ab
Manuel Sanchis Tormo <manuelsanchistormo@gmail.com>
2024-05-30 11:37:42 +0200

    clean go local module

--- src/app/go-local/go-local.module.ts
+++ src/app/go-local/go-local.module.ts
@@ -1,56 +1,61 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-import { FaqsModule } from './pages/faqs/faqs.module';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
 import { SharedModule } from '../shared/shared.module';
-import { BookGuideBtnComponent } from './components/book-guide-btn/book-guide-btn.component';
-import { CardGuideDestinationComponent } from './components/card-guide-destination/card-guide-destination.component';
-import { CardPostComponent } from './components/card-post/card-post.component';
-import { ErrorComponent } from './pages/error/error.component';
-import { GlCardGuidesComponent } from './components/gl-card-guides/gl-card-guides.component';
+import { FaqsModule } from './pages/faqs/faqs.module';
+
 import { HomeComponent } from './pages/home/home.component';
 import { SearchByDestinationComponent } from './pages/search-by-destination/search-by-destination.component';
-import { ItinerarioListComponent } from './components/itinerario-list/itinerario-list.component';
+import { ErrorComponent } from './pages/error/error.component';
 import { FichaGuiaComponent } from './pages/ficha-guia/ficha-guia.component';
 import { LoginComponent } from './pages/login/components/login.component';
 import { PerfilUsuarioComponent } from './pages/perfil-usuario/perfil-usuario.component';
 import { ArticuloComponent } from './pages/articulo/articulo.component';
-import { FormsModule } from '@angular/forms';
 import { BlogPageComponent } from './pages/blog/components/blog-pages.component';
-import { HttpClientModule } from '@angular/common/http';
 import { SignupComponent } from './pages/signup/signup.component';
+
+import { BookGuideBtnComponent } from './components/book-guide-btn/book-guide-btn.component';
+import { CardGuideDestinationComponent } from './components/card-guide-destination/card-guide-destination.component';
+import { CardPostComponent } from './components/card-post/card-post.component';
+import { GlCardGuidesComponent } from './components/gl-card-guides/gl-card-guides.component';
+import { ItinerarioListComponent } from './components/itinerario-list/itinerario-list.component';
 import { FichaReservaComponent } from './components/ficha-reserva/ficha-reserva.component';
-import { CancelModalComponent } from './components/ficha-reserva/modules/cancel-modal/cancel-modal.component';
+
 
 @NgModule({
   declarations: [
+    ArticuloComponent,
+    BlogPageComponent,
     BookGuideBtnComponent,
     CardGuideDestinationComponent,
     CardPostComponent,
     ErrorComponent,
+    FichaGuiaComponent,
+    FichaReservaComponent,
     GlCardGuidesComponent,
     HomeComponent,
     ItinerarioListComponent,
     LoginComponent,
     PerfilUsuarioComponent,
     SearchByDestinationComponent,
-    FichaGuiaComponent,
-    ArticuloComponent,
-    BlogPageComponent,
     SignupComponent,
-    FichaReservaComponent,
-    CancelModalComponent,
   ],
 
   imports: [
     CommonModule,
     FaqsModule,
     FormsModule,
+    FormsModule,
+    HttpClientModule,
     RouterModule,
     SharedModule,
-    HttpClientModule,
-    FormsModule,
   ],
-  exports: [GlCardGuidesComponent, HomeComponent],
+  exports: [
+    GlCardGuidesComponent,
+    HomeComponent,
+  ],
 })
 export class GoLocalModule {}
