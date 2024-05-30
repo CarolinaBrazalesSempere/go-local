3d998acbcaeb6d759844f9d95a518117e043dd7f
Manuel Sanchis Tormo <manuelsanchistormo@gmail.com>
2024-05-30 12:24:51 +0200

    clean faqs modal service

--- src/app/go-local/pages/faqs/services/faqs-modal.service.ts
+++ src/app/go-local/pages/faqs/services/faqs-modal.service.ts
@@ -2,14 +2,13 @@ import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class FaqsModalService {
-
   private showModalSource = new Subject<void>();
   showModal$ = this.showModalSource.asObservable();
 
-  constructor() { }
+  constructor() {}
 
   showModal() {
     this.showModalSource.next();
