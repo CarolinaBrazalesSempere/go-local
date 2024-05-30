6fbebacd48baaedf5c8e1f952f83a3dd9db9e93f
Manuel Sanchis Tormo <manuelsanchistormo@gmail.com>
2024-05-30 11:41:44 +0200

    clean app  module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { GoLocalModule } from './go-local/go-local.module';
-import { SharedModule } from './shared/shared.module';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { SharedModule } from './shared/shared.module';
+import { GoLocalModule } from './go-local/go-local.module';
+
+import { AppComponent } from './app.component';
 
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent,
+  ],
   imports: [
     AppRoutingModule,
     BrowserModule,
+    FormsModule,
     GoLocalModule,
     SharedModule,
-    FormsModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent],
+  providers: [
+
+  ],
+  bootstrap: [
+    AppComponent,
+  ],
 })
 export class AppModule {}
