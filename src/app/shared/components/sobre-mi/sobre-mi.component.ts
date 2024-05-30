361aa8a3c1b7cc43658ebffb4be00293a5472f5f
Manuel Sanchis Tormo <manuelsanchistormo@gmail.com>
2024-05-30 13:02:28 +0200

    clean sobre mi component

--- src/app/shared/components/sobre-mi/sobre-mi.component.ts
+++ src/app/shared/components/sobre-mi/sobre-mi.component.ts
@@ -1,15 +1,15 @@
 import { Component, Input } from '@angular/core';
+
 import { Itinerario } from 'src/app/go-local/interfaces/itinerario';
 
 @Component({
   selector: 'shared-sobre-mi',
   templateUrl: './sobre-mi.component.html',
-  styleUrls: ['./sobre-mi.component.css']
+  styleUrls: ['./sobre-mi.component.css'],
 })
 export class SobreMiComponent {
   @Input()
   sobreMi: string = '';
   @Input()
   itinerario!: Itinerario;
-
 }
